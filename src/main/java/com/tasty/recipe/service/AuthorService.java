bedf776ed760b4db65af59fc7eac720790663d8d
ferdinandova <ferdinandova.ivanaa@gmail.com>
2021-12-07 13:17:26 +0200

    Add service annotation

--- src/main/java/com/tasty/recipe/service/AuthorService.java
+++ src/main/java/com/tasty/recipe/service/AuthorService.java
@@ -3,7 +3,9 @@ package com.tasty.recipe.service;
 import com.tasty.recipe.entity.Author;
 import com.tasty.recipe.repository.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AuthorService {
     @Autowired
     private AuthorRepository authorRepository;
